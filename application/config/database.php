dc03fc4693a70a86de06b6730afbdedac2e106ea
jayvzolazo <jayvzolazo@gmail.com>
2013-10-26 17:25:43 +0800

    user credentials

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'mkyrrh_mkyuser';
+$db['default']['password'] = 'jv01Laz0!';
 $db['default']['database'] = 'mkyrrh_makaya';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
